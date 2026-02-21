3f4cf97db440bbab553f8d57a48a7df0f8a77e7d
Samantha Gallegos Perez <samanthagallegos814@gmail.com>
2026-02-20 21:58:34 -0800

    updated

--- Assignments/Gallegosperez_assignment2.py
+++ Assignments/Gallegosperez_assignment2.py
@@ -1,10 +1,16 @@
 from Bio import SeqIO
 
 
-
+#using the seq for the ref genome and store it as a string
 
 for record in SeqIO.parse("Assignment2_refgenome.fasta", "fasta"):
-    print (record.id)
+    seq = record.seq
+
+
+s_array = sorted([seq[i:] for i in range(len(seq))])
+
+s_array[3]
+
 
     
 
